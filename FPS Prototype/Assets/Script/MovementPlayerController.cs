6fa2ee20524d8c63474c1352b3ba67b4df758525
ntz448 <77314033+natizen448@users.noreply.github.com>
2021-10-12 19:44:12 +0900

    캐릭터 이동,애니메이션,사운드 80%

---
+++ FPS Prototype/Assets/Script/MovementPlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class MovementPlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;        //이동속도
+    private Vector3 moveForce;      //이동 힘 (x, z와 y축을 별도로 계산해 실제 이동에 적용)
+
+    private CharacterController characterController;
+    
+    public float MoveSpeed
+    {
+        set => moveSpeed = Mathf.Max(0, value);
+        get => moveSpeed;
+    }
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        //1초당 moveForce 속력으로 이동
+        characterController.Move(moveForce * Time.deltaTime);
+
+    }
+
+    public void MoveTo(Vector3 direction)
+    {
+        //이동 방향 = 캐릭터의 회전 값 * 방향 값
+        direction = transform.rotation * new Vector3(direction.x, 0, direction.z);
+
+        //이동 힘 = 이동방향 * 속도
+        moveForce = new Vector3(direction.x * moveSpeed, moveForce.y, direction.z * moveSpeed);
+    }
+}
